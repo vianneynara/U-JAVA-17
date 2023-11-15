49267f0bf150d28075999b3ecaf799127beaee24
narwa <narwandaru.nara@gmail.com>
2023-11-15 19:02:53 +0700

    Added safety by trimming the split results and parsing exception handling.

--- Section 12/src/challenge/Mappable.java
+++ Section 12/src/challenge/Mappable.java
@@ -5,15 +5,19 @@ public interface Mappable {
 	void render();
 
 	static double[] stringToLatLon(String coordinates) {
-		final String[] spliited = coordinates.split(",\\s");
+		final String[] spliited = coordinates.split(",");
 		if (spliited.length == 2) {
 			double[] converted = new double[2];
 			for (int i = 0; i < 2; i++) {
-				converted[i] = Double.parseDouble(spliited[i]);
+				try {
+					converted[i] = Double.parseDouble(spliited[i].trim());
+				} catch (NumberFormatException e) {
+					throw new IllegalArgumentException("Error parsing '" + spliited[i] + "' as a double value.");
+				}
 			}
 			return converted;
 		} else {
-			throw new IllegalArgumentException("The string passed is not properly formatted!");
+			throw new IllegalArgumentException("The passed string is not properly formatted!");
 		}
 	}
 }
