1a12b150d5b1f2b34b915cdfe902e6b608a2e685
narwa <narwandaru.nara@gmail.com>
2023-11-15 18:54:59 +0700

    Added constructors that receives the object name

--- Section 12/src/challenge/Park.java
+++ Section 12/src/challenge/Park.java
@@ -4,6 +4,10 @@ public class Park extends Point {
 
 	private String name;
 
+	public Park(String name) {
+		this.name = name;
+	}
+
 	public Park(String name, double[] location) {
 		super(location);
 		this.name = name;
--- Section 12/src/challenge/River.java
+++ Section 12/src/challenge/River.java
@@ -4,6 +4,10 @@ public class River extends Line {
 
 	private String name;
 
+	public River(String name) {
+		this.name = name;
+	}
+
 	public River(String name, double[][] locations) {
 		super(locations);
 		this.name = name;
